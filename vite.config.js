e41fd89879cbe435f2f43b1818596f5bf9c7befb
mugunthan17 <mugunthannagalingam17@gmail.com>
2025-02-23 22:42:42 +0530

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolioWebsite/",
+  base: "/portfolio-website/",
   build: {
     outDir: "dist",
     assetsDir: "assets"
